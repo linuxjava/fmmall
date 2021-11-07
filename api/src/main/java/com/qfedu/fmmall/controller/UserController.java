6db765ec05eef485e62fe315adb751165fd8e68b
robincxiao <robincxiao@tencent.com>
2021-11-07 20:14:24 +0800

    1.整合swagger 2.配置jrebel热部署

--- api/src/main/java/com/qfedu/fmmall/controller/UserController.java
+++ api/src/main/java/com/qfedu/fmmall/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.qfedu.fmmall.controller;
 
 import com.qfedu.fmmall.service.IUserService;
-import com.qfedu.fmmall.service.impl.UserService;
 import com.qfedu.fmmall.vo.ResultVo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,10 +20,13 @@ public class UserController {
     @Resource
     private IUserService userService;
 
-    @RequestMapping("/queryuser")
-    public ResultVo queryUser(@RequestParam("name") String name){
+    @ApiOperation("用户查询")
+    @ApiImplicitParams({
+            @ApiImplicitParam(dataType = "string", name = "name", required = true, value = "用户名")
+    })
+    @GetMapping("/queryuser")
+    public ResultVo queryUser(@RequestParam("name") String name) {
         System.out.println("name=" + name);
-
         return userService.queryUserByName(name);
     }
 }
--- common/src/main/java/com/qfedu/fmmall/vo/ResultVo.java
+++ common/src/main/java/com/qfedu/fmmall/vo/ResultVo.java
@@ -1,5 +1,7 @@
 package com.qfedu.fmmall.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,12 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@ApiModel(value = "ResultVO对象", description = "封装接口返回给前端的数据")
 public class ResultVo {
+    @ApiModelProperty(value = "相应状态码", dataType = "int")
     private int code;
+    @ApiModelProperty("响应提示信息")
     private String msg;
+    @ApiModelProperty("响应数据")
     private Object data;
 }
